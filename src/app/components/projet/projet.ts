9afacf6507aa5147f554c4d03e7dda64df07a70d
NDZESSAEMMANUEL <emmanuelfredy237@gmail.com>
2026-08-13 16:56:18 +0100

    chore:Initialisation du portfolio

---
+++ src/app/components/projet/projet.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projet',
+  imports: [],
+  templateUrl: './projet.html',
+  styleUrl: './projet.css',
+})
+export class Projet {}
