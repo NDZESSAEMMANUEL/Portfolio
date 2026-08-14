21b5fd7e12ad3fde06052f7214cb355e9c2c6bef
NDZESSAEMMANUEL <emmanuelfredy237@gmail.com>
2026-08-14 15:53:46 +0100

    feat:creation du composant Main

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import { About } from '../about/about';
+import { Contact } from '../contact/contact';
+import { Hero } from '../hero/hero';
+import { Projet } from '../projet/projet';
+import { Skills } from '../skills/skills';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [Header,Footer,About,Contact,Hero,Projet,Skills],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
